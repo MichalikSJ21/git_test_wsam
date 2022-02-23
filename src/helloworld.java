2d6d694e97a0b7530143f9639d69ef58777ac5fe
CLARKSC21 <scwclark21@gmail.com>
2022-02-23 14:45:37 -0500

    Updated to greet user by name

--- src/helloworld.java
+++ src/helloworld.java
@@ -6,6 +6,14 @@ public class helloworld {
         Scanner sc = new Scanner(System.in);
         String name = sc.next();
         System.out.println("hello "+ name);
-        System.out.println("your mom large");
+        System.out.println("Pick a num");
+        System.out.println(bestNumber(sc.nextInt()));
+    }
+
+    public static String bestNumber(int receive){
+        if(receive==69){
+            return "Hell yeah";
+        }
+        return "Wrong number, it's 69!!!!";
     }
 }
