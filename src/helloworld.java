1a8353db1f1e50753224bbfeb84e608dbf7d8510
CLARKSC21 <scwclark21@gmail.com>
2022-02-23 14:34:50 -0500

    Updated to greet user by name

--- src/helloworld.java
+++ src/helloworld.java
@@ -4,6 +4,7 @@ public class helloworld {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         String name = sc.next();
         System.out.println("hello "+ name);
     }
